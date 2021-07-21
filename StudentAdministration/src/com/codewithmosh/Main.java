de8b034d4facd2a1f8508b6686065c51b48eac8e
Amine <daviddefr@gmail.com>
2021-07-21 22:27:50 +0100

    I fixed the issue of while loop

--- StudentAdministration/src/com/codewithmosh/Main.java
+++ StudentAdministration/src/com/codewithmosh/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    var student = new Student();
-	    student.enrollCourse();
+        System.out.println(student.enrollCourse());
     }
 }
--- StudentAdministration/src/com/codewithmosh/Student.java
+++ StudentAdministration/src/com/codewithmosh/Student.java
@@ -34,6 +34,7 @@ public class Student {
         System.out.println("The details are :\nFirst name: " + firstName + "\nLast name: " + lastName + "\nThe ID : " + generatedID);
 
         //calling the enroll courses methode
+
     }
 
     //Generate the ID with the formula (5-digits with first number is the grade level "G-XXXX")
@@ -45,31 +46,34 @@ public class Student {
     //Enroll the courses
     public String enrollCourse () {
         int nbrOfCourses;
-        String choiceConfirmation = "";
+        String choiceConfirmation;
         ArrayList<String> coursesList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
-            System.out.println("Enter number of courses you want on the list bellow :\n1 - History101.\n2 - Mathematics101.\n3 - English101.\n4 - Chemistry101.\n5 - Computer Science101.");
-            nbrOfCourses = scanner.nextInt();
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+            while (true) {
+                System.out.println("Enter number of courses you want to enroll on the list bellow :\n1 - History101.\n2 - Mathematics101.\n3 - English101.\n4 - Chemistry101.\n5 - Computer Science101.\nNumber of courses : ");
+                nbrOfCourses = scanner1.nextInt();
             if (nbrOfCourses == 0){
                 System.out.println("Sorry! you entered number " + nbrOfCourses + " !");
             } else if (nbrOfCourses == 1) {
                 System.out.println("Your desire to enroll " + nbrOfCourses + " course! If you want to change the number of courses press C if you want to keep your choice press Y.");
-                choiceConfirmation = scanner.nextLine();
+                choiceConfirmation = scanner2.nextLine();
             } else {
-                do {
-                System.out.println("Your desire to enroll " + nbrOfCourses + " courses! If you want to change the number of courses press C if you want to keep your choice press Y.");
-                choiceConfirmation = scanner.nextLine();
-            } while (!choiceConfirmation.equals("y"));
-        }
-
-        for (int i = 0; i < nbrOfCourses; i++){
-            System.out.println("Enter the course n° " + (i + 1) + " to enroll." );
+            }
+                System.out.println("Your desire to enroll " + nbrOfCourses + " courses! If you want to change the number of courses press C if you want to keep your choice press Y :");
+                choiceConfirmation = scanner2.nextLine();
+                if (choiceConfirmation.equals("y")){
+                    break;
+                }
+            }
+        for (int i = 0; i < nbrOfCourses; i++) {
+            System.out.println("Enter the course n° " + (i + 1) + " to enroll.");
             String courses = scanner.nextLine();
             coursesList.add(courses);
             courseCost += 600;
-
         }
-        return "You chose " + nbrOfCourses + " as enroll course(s).\nThe courses are :\n" + coursesList + "\nThe cost of courses to pay is : $" + courseCost;
+        String result = "You chose " + nbrOfCourses + " as enroll course(s).\nThe courses are :\n" + coursesList + "\nThe cost of courses to pay is : $" + courseCost;
+        return result;
     }
 }
